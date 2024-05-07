7824db09d27e8c6665343958c3dfb9a3d767c242
wusuo666 <1600737233@qq.com>
2024-05-07 23:21:30 +0800

    master

--- ExtendedLinkListExperience/eightEmpress.cpp
+++ ExtendedLinkListExperience/eightEmpress.cpp
@@ -1,57 +1,71 @@
 #include<iostream>
+#include<vector>
+#include<cmath>
 using namespace std;
+//标记数组
+bool vis[30]={false};//列的标记数组，初始为false
+bool lr[30]={false};//左对角线的标记数组，初始为false 
+bool fr[30]={false};//右对角线的标记数组，初始为false 
 
-bool vis[30]={false};   //列的标记数组，初始为false
-bool lr[30]={false};    //左对角线的标记数组，初始为false
-bool fr[30]={false};    //右对角线的标记数组，初始为false
-const int len=8;
-int cnt=0;
-int data1[70]={0};
-int n=0;
+int cnt=0;//记录所有摆放方法的数量
+const int n = 8;
 
-bool judgeChess(int x,int y)
+
+bool judge(int x,int y)//判断当前位置是否能摆放皇后，当前位置为（x,y）
 {
-    if(vis[x]||lr[y-x+len]||fr[x+y]) return false;
-    else return true;
+    // cout<<"("<<vis[y]<<lr[y-x+n]<<fr[x+y]<<")";
+    if(vis[y] || lr[y-x+n] || fr[x+y])
+        return false;
+    return true;
 }
-
-void recursion_eight_empress(int y)      //递归方案
+int available(vector<int> v,int k)//if position k is available for chess
 {
-    cout<<n++<<"\t"<<y<<"\t";
-    if(y==8)
+    int pos = v.size(); 
+    for (int i = 0; i < v.size(); i++)
+        if (v[i] == k || (v[i] + i) == (k + pos) || (i - v[i]) == (pos - k))
+            return 0;
+ 
+    return 1;
+}
+void myshow(vector<int> v)
+{
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (v[i] == j)
+                cout << 'o';
+            else
+                cout << 'x';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+int play(vector<int> &data)
+{
+    int sum=0;
+
+    for(int i=0;i<8;i++)
     {
-        cout<<"第"<<++cnt<<"种摆法如下:"<<endl;
-        for(int i=0;i<len;i++)
+        if(available(data,i)&&data.size()<7)
         {
-            for(int j=0;j<len;j++)
-            {
-                cout<<data1[8*i+j]<<"\t";
-            }
-            cout<<endl;
-            return;
+            data.push_back(i);
+            sum+= play(data);
+            data.pop_back();
+        }
+        if(available(data,i)&&data.size()==7)
+        {
+            sum++;
+            data.push_back(i);
+            myshow(data);
+            data.pop_back();
         }
     }
-    for(int i=0;i<len;i++)
-    {
-        if(!judgeChess(i,y)) continue;
-        vis[i]=true;
-        lr[y-i+len]=true;
-        fr[y+i]=true;
-        data1[8*y+i]=true;
-        cout<<i<<"\t"<<y<<endl;
-        recursion_eight_empress(y+1);
-        vis[i]=false;
-        lr[y-i+len]=false;
-        fr[y+1];
-        data1[8*y+i]=false;
-
-    }
-    return;
+    return sum;
 }
-
 int main()
 {
-    recursion_eight_empress(1);
-
+    vector<int> queen;
+    cout<<play(queen)<<endl;
     return 0;
 }
+
